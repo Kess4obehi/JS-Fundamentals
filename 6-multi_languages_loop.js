84fd863d00d87bf2ff3e75d528d8f393349a4693
kess4obehi <kessiena4excellence@gmail.com>
2025-11-06 16:23:22 +0100

    correction made

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,4 +1,4 @@
-const array = ["c is fun", "Python is cool", "JavaScript is amazing"];
-for (i = 0; i < array.length; i++) {
+const array = ["C is fun", "Python is cool", "JavaScript is amazing"];
+for (let i = 0; i < array.length; i++) {
   console.log(array[i]);
 }
